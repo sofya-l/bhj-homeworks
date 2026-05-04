0affd4296374ebdf6d521769107e2d0e2c36b0e0
sof <122sonya122@gmail.com>
2026-05-04 11:39:29 +0300

    Переделанные 2 задания

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,20 +1,22 @@
 let dropdown = document.querySelector('.dropdown');
 let list = document.querySelector('.dropdown__list');
-let links = document.querySelectorAll('.dropdown__link');
 let currentValue = document.querySelector('.dropdown__value');
-let items = document.querySelectorAll('.dropdown__item');
 
-links.forEach((item) => item.addEventListener('click', link => {
-    link.preventDefault();
-    link.stopPropagation();
+dropdown.addEventListener('click', () => { 
+    list.classList.toggle('dropdown__list_active');
+});
+
+list.addEventListener('click', function(event) {
+    let item = event.target.closest('.dropdown__link');
+
+    event.preventDefault();
+    event.stopPropagation();
+    if(!item) return;
+
     if(list.classList.contains('dropdown__list_active')) {
         currentValue.textContent = item.textContent;
         list.classList.remove('dropdown__list_active'); 
-    }
-}));
-
-dropdown.addEventListener('click', () => { 
-    list.classList.add('dropdown__list_active');
-});
+    }   
+})
 
  
--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,16 +1,21 @@
-let tab = document.getElementsByClassName('tab');
-let content = document.getElementsByClassName('tab__content');
+let container = document.getElementById('tabs1');
+let tab = container.querySelectorAll('.tab');
+let content = container.querySelectorAll('.tab__content');
+
 
 for (let i = 0; i < tab.length; i++) {
     tab[i].addEventListener('click', function() {
-        let currentTab = document.querySelector('.tab_active');
-        let currentContent = document.querySelector('.tab__content_active');
+        let currentTab = container.querySelector('.tab_active');
+        let currentContent = container.querySelector('.tab__content_active');
 
-        currentTab.classList.remove('tab_active');
+        if(currentTab) {
+            currentTab.classList.remove('tab_active');
+        }
         tab[i].classList.add('tab_active');
-        currentContent.classList.remove('tab__content_active');
-        content[i].classList.add('tab__content_active');
 
+        if(currentContent) {
+            currentContent.classList.remove('tab__content_active');
+        }
+        content[i].classList.add('tab__content_active');
     });
 }
-
